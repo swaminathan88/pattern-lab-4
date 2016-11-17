7c1c2b566b8b6c63217b7d82c8d743b75baf76c6
Swami <swaminathan.g35@wipro.com>
2016-11-17 18:28:28 +0530

    homepage-done

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+
+
+function switchmenu(){
+	   var x = document.getElementById('aside-lists');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
